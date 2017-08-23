01ee40edf53cceba14314b7ec1fdcca4cb005fbe
hui_Tan <403554355@qq.com>
2017-08-23 16:30:38 +0800

    Initial Commit

---
+++ Trick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trick
+//
+//  Created by tanhui on 2017/8/23.
+//  Copyright © 2017年 tanhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Trick/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trick
+//
+//  Created by tanhui on 2017/8/23.
+//  Copyright © 2017年 tanhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
